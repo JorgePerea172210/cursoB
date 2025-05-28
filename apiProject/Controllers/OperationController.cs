0fc906ac5ff3a81d202ed8432f3b2cc26a19626a
JorgePerea172210 <ji.perea@dataware.com.mx>
2025-05-28 11:12:19 -0600

    Inicial commit

---
+++ apiProject/Controllers/OperationController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apiProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationController : ControllerBase
+    {
+        [HttpGet]
+        public decimal Get(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        [HttpPost]
+        public decimal Add(Numbers numbers, [FromHeader] string Host, [FromHeader(Name = "Content-Lenght")] string ContentLenght)
+        {
+            Console.WriteLine(Host);
+            Console.WriteLine("Content lenght: " + ContentLenght);
+            return numbers.A - numbers.B;
+        }
+
+        [HttpPut]
+        public decimal Edit(decimal a, decimal b)
+        {
+            return a * b;
+        }
+
+        [HttpDelete]
+        public decimal Delete(decimal a, decimal b)
+        {
+            return a / b;
+        }
+    }
+
+    public class Numbers
+    {
+        public decimal A { get; set; }
+        public decimal B { get; set; }
+    }
+}
---
+++ apiProject/Controllers/PeopleController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apiProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        [HttpGet("all")]
+        public List<People> GetPeople() => Repository.People;
+
+        [HttpGet("{id}")]
+        public People Get(int id) => Repository.People.First(p => p.Id == id);
+
+        [HttpGet("search/{word}")]
+        public List<People> Get(string word) =>
+            Repository.People.Where(p => p.Name.ToUpper().Contains(word.ToUpper())).ToList();
+    }
+
+    public class Repository
+    {
+        public static List<People> People = new List<People>
+        {
+            new People()
+            {
+                Id = 1, Name = "Jorge", Birthdate = new DateTime(2005, 3, 8)
+            },
+            new People()
+            {
+                Id = 2, Name = "Andrea", Birthdate=new DateTime(2005, 3, 23)
+            },
+            new People()
+            {
+                Id = 3, Name = "Sara", Birthdate = new DateTime(2004, 8, 16)
+            }
+        };
+    }
+
+    public class People
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public DateTime Birthdate { get; set; }
+
+
+    }
+}
